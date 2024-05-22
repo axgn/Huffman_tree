4113204b6b2c11467f826cc66407676d9febb6f9
Mona Lisa <3451962304@qq.com>
2024-05-22 20:38:13 +0800

    one_commit

---
+++ Huffman_tree/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<unordered_map>
+
+using namespace std;
+
+int main()
+{
+	string s_in;
+	bool running = true;
+	while (running)
+	{
+		s_in.clear();
+		getline(cin, s_in);
+		if (s_in == "0")
+		{
+			running = false;
+		}
+		else
+		{
+
+		}
+	}
+	return 0;
+}
